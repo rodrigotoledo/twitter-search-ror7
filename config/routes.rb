0cc33d65369c09c2645dd0da7bde5c5b68e0c492
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2022-02-06 01:47:51 -0300

    building basic layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'dashboard#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'dashboard/follwoing'
 end
